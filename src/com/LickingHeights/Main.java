b845bcfe10e2063e98495929ad124bba0c6fad81
LOPEZ <jordan.lopez.2022@student.lhschools.org>
2020-02-24 13:56:52 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,16 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("My number is ");
+        System.out.println("6083");
+        System.out.println("Brenda Will Come Over");
+        System.out.println("Build The Fence");
+        System.out.println("Go Into The Pool");
+
+
+    }
+
+}
